51a3f18a8d39f211388ccabbc123d1ce7a381ce7
zabronax <zabronax@gmail.com>
2025-09-16 10:45:43 +0200

    update task card

--- createTaskCard.js
+++ createTaskCard.js
@@ -1,9 +1,19 @@
 export function createTaskCard(task) {
+  // First create all the elements
   const taskCard = document.createElement("li")
-  
-  
-  taskCard.className = "task-item"
-  taskCard.textContent = task
+  const titleElement = document.createElement("p")
+  const imageElement = document.createElement("img")
 
+  // Configure the elements
+  taskCard.className = "task-item"
+  titleElement.textContent = task.title
+  imageElement.src =task.imageReference
+  imageElement.width = 30
+
+  // Organize them
+  taskCard.append(titleElement)
+  taskCard.append(imageElement)
+
+  // Return the complete card
   return taskCard
 }
--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 import { createTaskCard } from "./createTaskCard.js"
 
 const tasks = [
-  "Mince meat",
-  "Feed sharks",
-  "Take a stroll",
+  {
+    title: "Mince meat",
+    imageReference: "assets/mince-meat.avif"
+  },
+  {
+    title: "Feed sharks",
+    imageReference: "assets/mince-meat.avif"
+  },
+  {
+    title: "Take a stroll",
+    imageReference: "assets/mince-meat.avif"
+  }
 ]
 
 const taskList = document.querySelector("#task-list")
@@ -12,4 +21,3 @@ for (const task of tasks) {
   const card = createTaskCard(task)
   taskList.append(card)
 }
-
